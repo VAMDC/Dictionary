16068eb9acfaf9ce85c173ffe2627c55dbd8725e
Thomas Marquart <thomas@marquart.se>
2019-03-26 12:36:16 +0100

    urls

--- browse/urls.py
+++ browse/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
 
 from .views import *
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dictionary/intro.html')),
+    path('', TemplateView.as_view(template_name='browse/intro.html')),
     path('returnables/', returnables_by_type),
     path('restrictables/', restrictables),
     path('requestables/', requestables),
